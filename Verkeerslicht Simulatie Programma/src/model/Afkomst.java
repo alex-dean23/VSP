faf76340007e71c2c1a0bab71018ca7bbf9a5360
alex-dean23 <61474538+alex-dean23@users.noreply.github.com>
2020-08-26 20:51:48 -0300

    Add files via upload

---
+++ Verkeerslicht Simulatie Programma/src/model/Afkomst.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Afkomst {
+
+    public Voertuig voertuig;
+    public Wegdek wegdek;
+
+    Voertuig voertuig = new Voertuig ();
+    voertuig.setVoertuig("autoNumr" , "kentNunr", wegdekCode )
+
+
+    Wegdek wegdek = new wegdek ();
+    Wegdek.setWegdek("wegdekCode ", reverseVoertuigen)
