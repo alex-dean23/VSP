fd405fd9a162f58256e69612f4a137322ef4f391
Rwynn Christian <mitch935@hotmail.nl>
2020-08-26 22:11:41 -0300

    Resolved

--- Verkeerslicht Simulatie Programma/src/dstructures/Stack.java
+++ Verkeerslicht Simulatie Programma/src/dstructures/Stack.java
@@ -1,27 +1,18 @@
 package dstructures;
 
+import model.Afkomst;
+
 public class Stack {
   
   private int stackSize;
     private Afkomst[] stackArr;
     private int top;
- 
-    /**
-     * constructor to create stack with size
-     * @param size
-     */
+
     public Stack() {
-        this.stackSize = size;
         this.stackArr = new Afkomst[100];
         this.top = -1;
     }
- 
-    /**
-     * This method adds new entry to the top 
-     * of the stack
-     * @param entry
-     * @throws Exception 
-     */
+
     public void push(Afkomst entry){
         if(this.isStackFull()){
             System.out.println(("Stack is full. Increasing the capacity."));
@@ -30,13 +21,7 @@ public class Stack {
         System.out.println("Adding: "+entry);
         this.stackArr[++top] = entry;
     }
- 
-    /**
-     * This method removes an entry from the 
-     * top of the stack.
-     * @return
-     * @throws Exception 
-     */
+
     public Afkomst pop() throws Exception {
         if(this.isStackEmpty()){
             throw new Exception("Stack is empty. Can not remove element.");
@@ -45,18 +30,12 @@ public class Stack {
         System.out.println("Removed entry: "+entry);
         return entry;
     }
-     
-    /**
-     * This method returns top of the stack
-     * without removing it.
-     * @return
-     */
+
     public Afkomst peek() {
         return stackArr[top];
     }
  
     private void increaseStackCapacity(){
-         
         Afkomst[] newStack = new Afkomst[this.stackSize*2];
         for(int i=0;i<stackSize;i++){
             newStack[i] = this.stackArr[i];
@@ -64,20 +43,11 @@ public class Stack {
         this.stackArr = newStack;
         this.stackSize = this.stackSize*2;
     }
-     
-    /**
-     * This method returns true if the stack is 
-     * empty
-     * @return
-     */
+
     public boolean isStackEmpty() {
         return (top == -1);
     }
- 
-    /**
-     * This method returns true if the stack is full
-     * @return
-     */
+
     public boolean isStackFull() {
         return (top == stackSize - 1);
     }
